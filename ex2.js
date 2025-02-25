eccd16a55f03520f528a6853cac0a7dad5b8bad6
peyin192 <vuonglinh190206.qv@gmail.com>
2025-02-25 22:08:08 +0700

    Add files via upload

---
+++ ex2.js
@@ -0,0 +1,9 @@
+let number = 10;
+let price = 20.5;
+let statuss = true;
+let city = "Hà Nội";
+
+document.write(number); 
+document.write(price);
+document.write(statuss);
+document.write(city);
---
+++ ex3.js
@@ -0,0 +1,10 @@
+console.log("wellcome to Rikkei Academy");
+document.write("Quyết tâm học lập trình javascript");
+alert("Chào mừng bạn đến RIKKEI");
+var isConfirm = confirm("Bạn đã đủ 18 tuổi chưa?");
+if (isConfirm){
+    console.log("Roi");
+}else{
+    console.log("chua");
+}
+
